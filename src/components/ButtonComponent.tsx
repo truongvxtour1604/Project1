c72af630872bd41615a8567cbb70da59278edb2d
truongvxtour1604 <truongvxtour1604@gmail.com>
2025-04-29 21:38:27 +0700

    add react native icons

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleProp, ViewStyle, TextStyle, TouchableOpacity } from 'react-native'
+import React, { ReactNode } from 'react'
+import TextComponent from './TextComponent'
+import { globalStyles } from '../styles/globalStyles'
+import { appColors } from '../constants/appColor'
 
-const ButtonComponent = () => {
+interface Props {
+    icon?: ReactNode,
+    text: string,
+    type?: 'primary' | 'text' | 'link',
+    color?: string,
+    styles?: StyleProp<ViewStyle>,
+    textColor?: string,
+    textStyles?: StyleProp<TextStyle>,
+    onPress?: () => void,
+    iconFlex?: 'right' | 'left'
+}
+
+const ButtonComponent = (props: Props) => {
+
+    const { icon, text, type, color, styles, textColor, textStyles, onPress, iconFlex } = props
+
     return (
-        <View>
-            <Text>ButtonComponent</Text>
-        </View>
+        type === 'primary' ? (
+
+            <TouchableOpacity
+                onPress={onPress}
+                style={[
+                    globalStyles.button, {
+                        backgroundColor: color ?? appColors.primary
+                    }, styles
+                ]}>
+                {icon && icon}
+                <TextComponent
+                    text={text}
+                    color={textColor ?? appColors.white}
+                    styles={[textStyles, { marginLeft: icon && iconFlex === 'left' ? 12 : 0 }]}
+                    flex={icon && iconFlex === 'right' ? 1 : 0}
+                />
+                {icon && iconFlex === 'right' && icon}
+            </TouchableOpacity>
+        ) : (
+            <TouchableOpacity>
+                <TextComponent
+                    text={text}
+                    color={type === 'link' ? appColors.primary : appColors.text}
+                />
+            </TouchableOpacity>
+        )
     )
 }
 
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,12 +1,16 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { ButtonComponent } from '../../components'
+import { appColors } from '../../constants/appColor'
+import { globalStyles } from '../../styles/globalStyles'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-      <Button title='Login' onPress={async () => await AsyncStorage.setItem('assetToken', 'loginSuccess')} />
+    <View style={[globalStyles.container, {}]}>
+      {/* <Text>LoginScreen</Text>
+      <Button title='Login' onPress={async () => await AsyncStorage.setItem('assetToken', 'loginSuccess')} /> */}
+      <ButtonComponent text='Login' icon={<View><Text></Text></View>} type='primary'/>
     </View>
   )
 }
